db713e15b560fbe8f9ab03f8af195c44e852c5c7
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-22 15:05:08 +0530

    fix: Update main.dart to use const constructor for MyApp and correct home screen reference

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:notepad/screens/notes_list_screen.dart';
+import 'screens/notes_list_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Notepad',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const NoteListScreen(),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const NotesListScreen(),
     );
   }
 }
-
-
