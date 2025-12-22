e4234c74cb2292cbac14ac095cd44f4e75e836a2
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-22 14:41:15 +0530

    feat: Implement NoteListScreen with note loading and display functionality

--- lib/screens/notes_list_screen.dart
+++ lib/screens/notes_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notepad/db/note_database.dart';
+import '../models/note.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -8,8 +10,56 @@ class NoteListScreen extends StatefulWidget {
 }
 
 class _NoteListScreenState extends State<NoteListScreen> {
+  List<Note> notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadNotes();
+  }
+
+  Future<void> loadNotes() async {
+    notes = await NoteDatabase.instance.readAllNotes();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => const NoteListScreen()),
+          );
+          loadNotes();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: notes.isEmpty
+          ? const Center(child: Text("No notes yet"))
+          : ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final note = notes[index];
+                return ListTile(
+                  title: Text(note.title),
+                  subtitle: Text(
+                    note.content,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  onTap: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const NoteListScreen()),
+                    );
+                    loadNotes();
+                  },
+                );
+              },
+            ),
+    );
   }
 }
