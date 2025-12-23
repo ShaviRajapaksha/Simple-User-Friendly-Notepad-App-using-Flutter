eb0cbc9b194837aec33f4f943ed7ae5b3a337d15
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-23 20:37:20 +0530

    feat: Enhance NotesListScreen with improved UI and note display details

--- lib/screens/notes_list_screen.dart
+++ lib/screens/notes_list_screen.dart
@@ -3,7 +3,7 @@ import 'package:notepad/screens/notes_editor_screen.dart';
 import 'package:notepad/screens/notes_view_screen.dart';
 import '../db/note_database.dart';
 import '../models/note.dart';
-
+import 'package:intl/intl.dart';
 
 class NotesListScreen extends StatefulWidget {
   const NotesListScreen({super.key});
@@ -29,8 +29,14 @@ class _NotesListScreenState extends State<NotesListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Notes')),
-      floatingActionButton: FloatingActionButton(
+      appBar: AppBar(
+        title: const Text(
+          'My Notes',
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
           await Navigator.push(
             context,
@@ -38,30 +44,74 @@ class _NotesListScreenState extends State<NotesListScreen> {
           );
           loadNotes();
         },
-        child: const Icon(Icons.add),
+        icon: const Icon(Icons.add),
+        label: const Text('New Note'),
       ),
       body: notes.isEmpty
-          ? const Center(child: Text('No notes yet'))
+          ? const Center(
+              child: Text(
+                'No notes yet.',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            )
           : ListView.builder(
+              padding: const EdgeInsets.all(12),
               itemCount: notes.length,
               itemBuilder: (context, index) {
                 final note = notes[index];
-                return ListTile(
-                  title: Text(note.title),
-                  subtitle: Text(
-                    note.content,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+
+                return Card(
+                  elevation: 3,
+                  color: const Color.fromARGB(255, 236, 221, 251),
+                  margin: const EdgeInsets.only(bottom: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
                   ),
-                  onTap: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => NoteViewScreen(note: note),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => NoteViewScreen(note: note),
+                        ),
+                      );
+                      loadNotes();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            note.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            note.content,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: Colors.grey.shade700),
+                          ),
+                          const SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              DateFormat('dd MMM yyyy').format(note.createdAt),
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    );
-                    loadNotes();
-                  },
+                    ),
+                  ),
                 );
               },
             ),
